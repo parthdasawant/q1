e42942241b4f65033d1494747f4aebffe67d7d23
parthdasawant <parthdasawant07@gmail.com>
2024-07-12 09:28:15 +0530

    Added answer of the question 1

---
+++ q1_ans.py
@@ -0,0 +1,11 @@
+# Python program to add two numbers provided by the user
+
+# Take two numbers from the user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Calculate the sum
+sum = num1 + num2
+
+# Display the sum
+print("The sum of {0} and {1} is {2}".format(num1, num2, sum))
